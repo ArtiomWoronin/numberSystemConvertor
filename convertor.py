38a30694572b1d49d7c9fdb7d3d8f3876a4abdce
Artiom Woronin <62155514+ArtiomWoronin@users.noreply.github.com>
2024-03-26 12:15:20 +0000

    Update convertor.py

--- convertor.py
+++ convertor.py
@@ -1,19 +1,19 @@
 numRaw = input("Enter your number using decimal number system: ").replace(' ', '')
 def factors(x):
 	factorList = []
-	for d in range(2,x//2):
-		if x%d==0:
+	for d in range(2,x // 2):
+		if x % d == 0:
 			factorList.append(d)
-	factorList+=[x//2,x]
+	factorList += [x // 2,x]
 	return factorList
 
 
 
-if '.' in numRaw and numRaw[0]!='.':
+if '.' in numRaw and numRaw[0] != '.':
 	numRaw = numRaw.split('.')
 	number = int(numRaw[0])
 	period = numRaw[1]
-elif numRaw[0]=='.':
+elif numRaw[0] == '.':
 	number = 0
 	period = numRaw[1::]
 else:
@@ -23,10 +23,10 @@ numSys = int(input("What number system do you want to convert this number in?(mu
 
 
 
-if numSys==1 and period == '':
-	print('1'*number, '\nAre you good now?')
+if numSys == 1 and period == '':
+	print('1' * number, '\nAre you good now?')
 	quit()
-elif (numSys!=int or numSys==1) and period == '':
+elif (numSys != int or numSys == 1) and period == '':
 	print("Wrong value!")
 	quit()
 
@@ -37,102 +37,103 @@ digitDict = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 
 
-if number!=0:
-	remainder = number%numSys
+if number != 0:
+	remainder = number % numSys
 	n = 0
 	numOut = ''
 	numOutPeriodRaw = ''
-	if numSys<=36:
-		while number!=0:
-			digits.insert(0, digitDict[number%numSys])
-			number = number//numSys
+	if numSys <= 36:
+		while number != 0:
+			digits.insert(0, digitDict[number % numSys])
+			number = number // numSys
 	else:
-		while number!=0:
-			digits.insert(0, '(' + str(number%numSys) + ')')
-			number = number//numSys
+		while number != 0:
+			digits.insert(0, '('  +  str(number % numSys)  +  ')')
+			number = number // numSys
 	numOut = ''.join(digits)
 else:
 	numOut = '0'
 
 
 
-if period !='':
+if period != '':
 	numOutPeriodRaw = ''
 	periodLen = len(period)
 	power = input("Please, enter your fractional precision: ")
 	roundFlag = input("Round? Y/n ").lower()
 
-	if ('max' in power) and numSys<10:
-		power = len(period)*len(period)+1
-	elif ('max' in power) and numSys>=10:
+
+	if ('max' in power) and numSys < 10:
+		power = len(period) * len(period) + 1
+	elif ('max' in power) and numSys >= 10:
 		if 2 in factorList and 5 in factorList:
 			power = len(period)
 		else:
-			power = len(period)*len(period)+1
+			power = len(period) * len(period) + 1
 	else:
 		power = int(power)
 
 
 
-if period == '' or power<1:
+if period == '' or power < 1:
 	numOutPeriodRaw = ''
 
-elif period!='0' and numSys<=36:
+elif period != '0' and numSys <= 36:
 	i = 0
 	numOutPeriodRaw = ''
-	while i<=power:
-		periodRaw = str(int(period)*numSys)[:-periodLen:]
-		if periodRaw!='':
+	while i <= power:
+		periodRaw = str(int(period) * numSys)[:-periodLen:]
+		if periodRaw != '':
 			numOutPeriodRaw += digitDict[int(periodRaw)]
 		else:
 			numOutPeriodRaw += '0'
-		period = str(int(period)*numSys)[-periodLen::]
-		i+=1
-	numOutPeriodRaw = '.'+numOutPeriodRaw
+		period = str(int(period) * numSys)[-periodLen::]
+		i += 1
+	numOutPeriodRaw = '.' + numOutPeriodRaw
 
-elif period!='' and numSys>36:
+elif period != '' and numSys > 36:
 	i = 0
 	numOutPeriodRaw = ''
-	while i<=power:
-		periodRaw = str(int(period)*numSys)[:-periodLen:]
-		if periodRaw!='':
+	while i <= power:
+		periodRaw = str(int(period) * numSys)[:-periodLen:]
+		if periodRaw != '':
 			numOutPeriodRaw += '(' + periodRaw + ')'
 		else:
 			numOutPeriodRaw += '(0)'
-		period = str(int(period)*numSys)[-periodLen::]
-		i+=1
-	numOutPeriodRaw = '.'+numOutPeriodRaw
+		period = str(int(period) * numSys)[-periodLen::]
+		i += 1
+	numOutPeriodRaw = '.' + numOutPeriodRaw
 
 if 'y' in roundFlag:
 	numOutPeriodRaw = list(numOutPeriodRaw)
-	if numSys%2==0:
-		numHalf = numSys//2
+	if numSys%2 == 0:
+		numHalf = numSys // 2
 	else:
-		numHalf = numSys//2 + 1
-	if numSys<=36:
-		for i in range(len(numOutPeriodRaw)-1,1,-1):
-			if digitDict.index(numOutPeriodRaw[i])>=numHalf:
-				numOutPeriodRaw[i-1] = digitDict[digitDict.index(numOutPeriodRaw[i-1])+1]
+		numHalf = numSys // 2 + 1
+	if numSys <= 36:
+		for i in range(len(numOutPeriodRaw)-1, 1, -1):
+			if digitDict.index(numOutPeriodRaw[i]) >= numHalf:
+				numOutPeriodRaw[i-1] = digitDict[digitDict.index(numOutPeriodRaw[i-1]) + 1]
 				numOutPeriodRaw[i] = ''
-				print(numOut+''.join(numOutPeriodRaw))
+				print(numOut + ''.join(numOutPeriodRaw))
 				quit()
 			else:
 				numOutPeriodRaw[i] = ''
 	else:
-		for i in range(len(numOutPeriodRaw)-1,1,-1):
-			if int(numOutPeriodRaw[i].replace('(','').replace(')',''))>=numHalf:
+		for i in range(len(numOutPeriodRaw)-1, 1, -1):
+			if int(numOutPeriodRaw[i].replace('(','').replace(')','')) >= numHalf:
 				numOutPeriodRaw[i-1] = str(int(numOutPeriodRaw[i-1].replace('(','').replace(')','')) + 1)
 				numOutPeriodRaw[i] = ''
-				print(numOut+''.join(numOutPeriodRaw))
+				print(numOut + ''.join(numOutPeriodRaw))
 				quit()
 			else:
 				numOutPeriodRaw[i] = ''
 
 
 
-if len(numOut+numOutPeriodRaw)<5000:
-	print(numOut+numOutPeriodRaw)
+if len(numOut + numOutPeriodRaw) < 5000:
+	print(numOut + numOutPeriodRaw)
 else:
 	wToBiNu = open(r'/home/artyom/Desktop/wayTooBigNumber.txt', 'w+')
-	wToBiNu.write(numOut+numOutPeriodRaw)
+	wToBiNu.write(numOut + numOutPeriodRaw)
 	wToBiNu.close()
